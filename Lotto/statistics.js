15087051eed1ec92c3a2ee20b0c20bdfdad2efb0
Waxxa_dj <mosqalejesus@gmail.com>
2022-07-12 23:17:18 -0500

    more progres in lotto

--- Lotto/statistics.js
+++ Lotto/statistics.js
@@ -145,7 +145,83 @@ const mercoledi = [
         [1,1]
     ]
 ]
-const ocacions =[
+let last_domenica = [
+    [1,4],
+    [6,5],
+    [7,6],
+    [1,5],
+    [8,0],
+    [1,3],
+    [0,8],
+    [3,7],
+    [7,6],
+    [2,0],
+    [1,6],
+    [5,7],
+    [0,3],
+    [4,7],
+    [7,4],
+    [1,2],
+    [9,4],
+    [4,9],
+    [4,6],
+    [3,2]
+];
+let last_mercoledi = [
+    [2,9],
+    [2,7],
+    [0,0],
+    [1,5],
+    [8,0],
+    [1,3],
+    [0,8],
+    [3,7],
+    [7,6],
+    [2,0],
+    [1,6],
+    [5,7],
+    [0,3],
+    [4,7],
+    [7,4],
+    [1,2],
+    [9,4],
+    [4,9],
+    [4,6],
+    [3,2],
+]
+let sum = [
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0]
+];
+function mainS (group, tot) {
+    for(let i = 0; i < group.length; i++) {
+        for (let j = 0; j < group[i].length; j++) {
+        (group[i][j] == 0) ? tot[0][j]++
+            : (group[i][j] == 1) ? tot[1][j]++
+            : (group[i][j] == 2) ? tot[2][j]++
+            : (group[i][j] == 3) ? tot[3][j]++
+            : (group[i][j] == 4) ? tot[4][j]++
+            : (group[i][j] == 5) ? tot[5][j]++
+            : (group[i][j] == 6) ? tot[6][j]++
+            : (group[i][j] == 7) ? tot[7][j]++
+            : (group[i][j] == 8) ? tot[8][j]++
+            : tot[9][j]++;
+        }
+    }
+    console.log(tot);
+    console.log('sum');
+}
+mainS(last_domenica,sum);
+
+let ocacions = [
     [0,0],
     [0,0],
     [0,0],
@@ -157,28 +233,45 @@ const ocacions =[
     [0,0],
     [0,0]
 ]
-function cont (days, times) {
+let test_dec = [];
+let test_und = [];
+function cont (days, times, testD, testM) {
     for(let i = 1; i < days.length; i++) {
         for(let j = 0; j < days[i].length ; j++) {
+            console.log(days[i][j].length);
             for(let k = 0; k < days[i][j].length; k++) {
-                (days[i][j][k] == 0) ? times[0][k] = times[0][k] + 1
-                : (days[i][j][k] == 1) ? times[1][k] = times[1][k] + 1
-                : (days[i][j][k] == 2) ? times[2][k] = times[2][k] + 1
-                : (days[i][j][k] == 3) ? times[3][k] = times[3][k] + 1
-                : (days[i][j][k] == 4) ? times[4][k] = times[4][k] + 1
-                : (days[i][j][k] == 5) ? times[5][k] = times[5][k] + 1
-                : (days[i][j][k] == 6) ? times[6][k] = times[6][k] + 1
-                : (days[i][j][k] == 7) ? times[7][k] = times[7][k] + 1
-                : (days[i][j][k] == 8) ? times[8][k] = times[8][k] + 1
-                : times[9][k] = times[9][k] + 1;
+                (days[i][j][k] == 0) ? times[0][k]++
+                : (days[i][j][k] == 1) ? times[1][k]++
+                : (days[i][j][k] == 2) ? times[2][k]++
+                : (days[i][j][k] == 3) ? times[3][k]++
+                : (days[i][j][k] == 4) ? times[4][k]++
+                : (days[i][j][k] == 5) ? times[5][k]++
+                : (days[i][j][k] == 6) ? times[6][k]++
+                : (days[i][j][k] == 7) ? times[7][k]++
+                : (days[i][j][k] == 8) ? times[8][k]++
+                : times[9][k]++;
+                console.trace();
             }
         }
+        console.table(times[9]);
         console.table(times);
-        for (let l = 0; l < times.length; l++) {
-            for (let m = 0; m < times[l].length; m++) {
+        let tsum = 0;
+        for(let l = 0; l < times.length; l++) {
+            for(var m = 0; m < times[l].length; m++) {
+                if(times[l][m] == 0 || times[l][m] == 1) {
+                    for(let n = 3; n < 0; n--) {
+                        console.log(m);
+                        console.log(days[i-1][n]);
+                        (days[i--][n][m] == times[l][m]) ? tsum++ : tsum += 0;
+
+                    }
+                    if(m = 0) {
+
+                    }
+                }
                 times[l][m] = 0;
             }
         }
     }
 }
-cont(domenica, ocacions);
+cont(domenica, ocacions,last_domenica,last_mercoledi);
