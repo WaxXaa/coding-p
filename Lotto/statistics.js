7c23882dc05cf8d0ff0c8b47da30c0f8fec856a5
Waxxa_dj <mosqalejesus@gmail.com>
2022-07-15 23:14:58 -0500

    lotto change

--- Lotto/statistics.js
+++ Lotto/statistics.js
@@ -70,125 +70,29 @@ const domenica = [
         [1,6],
         [8,6]
     ]
-]
-
-const mercoledi = [
-    [
-        [2,9],
-        [2,7],
-        [0,0]
-    ],
-    [
-        [8,1],
-        [2,2],
-        [1,4],
-        [0,1],
-        [4,0],
-        [4,0],
-        [3,8],
-        [6,6],
-        [5,0],
-        [0,1],
-        [1,0],
-        [5,1],
-        [3,9],
-        [0,2],
-        [6,7],
-        [7,1],
-        [9,0],
-        [1,6],
-        [7,6],
-        [1,7]
-    ],
-    [
-        [3,4],
-        [6,2],
-        [1,6],
-        [2,6],
-        [4,3],
-        [5,4],
-        [3,0],
-        [8,2],
-        [6,0],
-        [9,3],
-        [6,3],
-        [3,2],
-        [0,9],
-        [9,6],
-        [6,5],
-        [9,9],
-        [7,8],
-        [9,0],
-        [4,2],
-        [0,6]
-    ],
-    [
-        [8,7],
-        [5,8],
-        [8,7],
-        [1,4],
-        [7,7],
-        [7,1],
-        [3,8],
-        [1,3],
-        [5,4],
-        [9,7],
-        [0,5],
-        [4,2],
-        [5,2],
-        [2,5],
-        [7,1],
-        [6,2],
-        [5,5],
-        [5,9],
-        [9,1],
-        [1,1]
-    ]
-]
-let last_domenica = [
-    [1,4],
-    [6,5],
-    [7,6],
-    [1,5],
-    [8,0],
-    [1,3],
-    [0,8],
-    [3,7],
-    [7,6],
-    [2,0],
-    [1,6],
-    [5,7],
-    [0,3],
-    [4,7],
-    [7,4],
-    [1,2],
-    [9,4],
-    [4,9],
-    [4,6],
-    [3,2]
 ];
-let last_mercoledi = [
-    [2,9],
-    [2,7],
-    [0,0],
-    [1,5],
-    [8,0],
-    [1,3],
-    [0,8],
-    [3,7],
-    [7,6],
-    [2,0],
-    [1,6],
-    [5,7],
-    [0,3],
-    [4,7],
-    [7,4],
-    [1,2],
-    [9,4],
-    [4,9],
-    [4,6],
-    [3,2],
-]
+let last_dom =[
+        [1,4],
+        [6,5],
+        [7,6],
+        [1,5],
+        [8,0],
+        [1,3],
+        [0,8],
+        [3,7],
+        [7,6],
+        [2,0],
+        [1,6],
+        [5,7],
+        [0,3],
+        [4,7],
+        [7,4],
+        [1,2],
+        [9,4],
+        [4,9],
+        [4,6],
+        [3,2]
+];
 let sum = [
     [0,0],
     [0,0],
@@ -201,7 +105,9 @@ let sum = [
     [0,0],
     [0,0]
 ];
+
 function mainS (group, tot) {
+  console.log("numero | cantidad decenas | cantidad unidades");
     for(let i = 0; i < group.length; i++) {
         for (let j = 0; j < group[i].length; j++) {
         (group[i][j] == 0) ? tot[0][j]++
@@ -216,11 +122,10 @@ function mainS (group, tot) {
             : tot[9][j]++;
         }
     }
-    console.log(tot);
-    console.log('sum');
 }
-mainS(last_domenica,sum);
-
+mainS(last_dom,sum);
+console.log("\nGrupo principal");
+console.table(sum);
 let ocacions = [
     [0,0],
     [0,0],
@@ -235,10 +140,10 @@ let ocacions = [
 ]
 let test_dec = [];
 let test_und = [];
-function cont (days, times, testD, testM) {
+function cont (days, times, testd, testu) {
+  console.log("\n\nOTROS DATOS")
     for(let i = 1; i < days.length; i++) {
         for(let j = 0; j < days[i].length ; j++) {
-            console.log(days[i][j].length);
             for(let k = 0; k < days[i][j].length; k++) {
                 (days[i][j][k] == 0) ? times[0][k]++
                 : (days[i][j][k] == 1) ? times[1][k]++
@@ -250,28 +155,43 @@ function cont (days, times, testD, testM) {
                 : (days[i][j][k] == 7) ? times[7][k]++
                 : (days[i][j][k] == 8) ? times[8][k]++
                 : times[9][k]++;
-                console.trace();
             }
         }
-        console.table(times[9]);
-        console.table(times);
+        console.log("grupo", i);
+        console.log(times);
         let tsum = 0;
         for(let l = 0; l < times.length; l++) {
             for(var m = 0; m < times[l].length; m++) {
-                if(times[l][m] == 0 || times[l][m] == 1) {
-                    for(let n = 3; n < 0; n--) {
-                        console.log(m);
-                        console.log(days[i-1][n]);
-                        (days[i--][n][m] == times[l][m]) ? tsum++ : tsum += 0;
-
+                if(times[l][m] == 0 || times[l][m] == 1){
+                  for(let n = 3; days[i - 1].length - n < days[i - 1].length; n--) {
+                    if(days[i - 1][days[i - 1].length - n][m] == l) {
+                       if( m == 0) {
+                        tsum++;
+                        testd.push([l, tsum]);
+                       } else if(m == 1) {
+                        tsum++;
+                        testu.push([l, tsum])
+                       }
                     }
-                    if(m = 0) {
-
-                    }
+                  }
+                 tsum = 0;
                 }
                 times[l][m] = 0;
             }
         }
+        console.log("estas son la cantidades de veces que han aparecido los numeros menos jugados del grupo "+ i +" en los 3 ultimas filas del grupo anterior");
+        console.log("Columnas de las decenas\n");
+        console.table(testd);
+        console.log("Columnas de las unidades\n");
+        console.table(testu);
+        let borrar = (arr) => {
+            for(let i = 0; i < arr.length; i++) {
+                arr.splice(i);
+            }
+        };
+        borrar(testd);
+        borrar(testu);
     }
 }
-cont(domenica, ocacions,last_domenica,last_mercoledi);
+
+cont(domenica, ocacions, test_dec, test_und);
